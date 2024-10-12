5bae9ae85d8ccba22433cb9671cba38e047ee0f3
Zeid Tayyan <zaltayan.ieu2022@student.ie.edu>
2024-10-12 17:27:27 +0200

    added country and deployment

---
+++ src/components/HomePage.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+    <h1>Welcome to IE Banking</h1>
+    <router-link to="/accounts" class="link-button">Accounts</router-link>
+    <router-link to="/skull" class="link-button">Skull</router-link>
+    </div>
+</template>
